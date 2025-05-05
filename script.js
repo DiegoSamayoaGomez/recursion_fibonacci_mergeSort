38e084daaf6828e181a680f2f5578717cb83f063
DiegoSamayoaGomez <samayoasg2014@gmail.com>
2025-05-04 22:53:16 -0600

    Delete unused files

--- script.js
+++
@@ -1,9 +0,0 @@
-function fibonacci(numberToCalculate) {
-  if (numberToCalculate < 2) {
-    return numberToCalculate;
-  } else {
-    return fibonacci(numberToCalculate - 1) + fibonacci(numberToCalculate - 2);
-  }
-}
-
-console.log(fibonacci(5));
